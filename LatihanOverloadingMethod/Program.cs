315512f3783192d0313367945ea7ba3b7a0db03b
fafan ramadhan <forweare7@gmail.com>
2022-04-06 18:52:01 +0700

    Add project files.

---
+++ LatihanOverloadingMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LatihanOverloadingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers number = new Numbers();
+
+            Console.WriteLine("Minimum #1: {0}", number.FindingMinimum(3, 1));
+            Console.WriteLine("Minimum #2: {0}", number.FindingMinimum(3, 2, 4));
+            Console.WriteLine();//baris kosong
+            Console.WriteLine("Maximum #1: {0}", number.FindMaximum(3,1));
+            Console.WriteLine("Maximum #2: {0}", number.FindMaximum(3,2,4));
+
+            Console.ReadKey();
+        }
+    }
+}
